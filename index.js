96c94ca4946c8b65b04e59371041d91ea476087a
Tristan Tritch <ttmeep101@hotmail.com>
2024-10-07 10:51:33 -0500

    complete tests

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+const num1= 31;
+const num2 = 2;
+const num3 = 24;
+const num4 = 20;
+var multiply = num1*num2;
+var random = Math.floor(Math.random() * 10) + 1;
+
+var mod = num3%num4;
+var max = Math.max(1,2,4,20);
